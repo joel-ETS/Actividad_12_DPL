9aa4194b21dcd5f07f3d4438a55163ce1a2a5daf
joel <joelrafael24@gmail.com>
2023-02-02 17:09:17 +0000

    update commit

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Formulario de </title>
+        <title>Formulario  </title>
         <meta charset = "UTF-8">
         <script type = "text/javascript" src = "js/cargarDatos.js"></script>		
         <script type = "text/javascript" src = "js/sesion.js"></script>				
